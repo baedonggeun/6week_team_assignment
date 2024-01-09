3ff01ec25ba0f1b00d3ae7c836440f56e36cbd50
baedonggeun <hispanie3@gmail.com>
2024-01-09 12:29:05 +0900

    프로젝트 생성

---
+++ 6week_team_assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6week_team_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
